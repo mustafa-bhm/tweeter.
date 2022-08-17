3f95e951636525c2972638dfe21d71e75de4a815
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-17 22:21:12 +0000

    fix a bug with mentor Andrea M.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,47 +38,47 @@ $(document).ready(function () {
   </section>`;
     return $tweet;
   };
-  //// Form Submission using JQuery
-  $("form").on("submit", function (event) {
-    event.preventDefault();
-    // alert("nooooorefresh !");
-    // console.log("no refresh");
-    const newTweet = $(this).serialize();
+  //// rendering tweets
 
-    $.ajax({
-      url: "http://localhost:8080/tweets/",
-      method: "POST",
-      data: newTweet,
-      success: function (data) {
-        console.log("success");
-        console.log(this.data);
-        $("#tweet-text").val(""); /// to clear tweet text area
-        loadTweets();
-      },
-    });
-  });
   const renderTweets = function (dataBase) {
     //// rendering tweets
     console.log(dataBase);
+    $(".tweets-container").empty();
     dataBase.forEach((tweets) => {
       const $tweet = createTweetElement(tweets);
-      $(".tweets-container").append($tweet);
+      $(".tweets-container").prepend($tweet);
     });
   };
-
   /// Loading/fetching tweets from /tweets/
   const loadTweets = function () {
     $.ajax({
-      url: "http://localhost:8080/tweets/",
+      url: "/tweets/",
       method: "GET",
     })
       .then((response) => {
         renderTweets(response);
-        console.log("====");
+        console.log("==== fetching tweets ");
       })
       .catch((error) => {
         console.log("error:", error);
       });
   };
   loadTweets();
+
+  //// Form Submission using JQuery
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+
+    const newTweet = $(this).serialize();
+
+    $.ajax({
+      url: "/tweets/",
+      method: "POST",
+      data: newTweet,
+      success: function (data) {
+        $("#tweet-text").val(""); /// to clear  text area
+        loadTweets();
+      },
+    });
+  });
 });
